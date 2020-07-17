7693db3e7aa5f5c22abaab27ebf3cbf0ac62b5c9
maximuse <maximuse@windowslive.com>
2020-07-17 11:56:21 +0200

    Initial commit

--- src/hu/nyirszikszi/Main.java
+++ src/hu/nyirszikszi/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Csudh> list = Actions.fileToList("csudh.txt");
 
-        System.out.println("3. feladat: Domain-ek száma: " + Actions.task3(list));
-        Actions.task5(list, 0);
+        System.out.println("3. feladat: Domain-ek száma: " + Actions.task3());
+        Actions.task5(0);
         Actions.task6(list, "table.html");
     }
 }
